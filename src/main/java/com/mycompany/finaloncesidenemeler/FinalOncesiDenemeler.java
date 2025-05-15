3e738d5bfda70886983c50dbfdd7d7845184f6a6
Alperen SIMSEK <alperensimsek.asch@gmail.com>
2025-05-15 23:46:24 +0300

    deneme1
    deneme1.1

---
+++ src/main/java/com/mycompany/finaloncesidenemeler/FinalOncesiDenemeler.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finaloncesidenemeler;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alperen
+ */
+public class FinalOncesiDenemeler {
+
+    public static void main(String[] args) {
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Göndermek istediğiniz mesajı yazınız");
+        String mesaj = scanner.nextLine();
+        System.out.println(mesaj);
+        */
+        /*
+        int num1 = 42;
+        System.out. println(num1); //42
+        num1 = 25;
+        System.out.println(num1);  // 25
+        */
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a boolean ");
+        boolean value = Boolean.valueOf(scanner.nextLine());
+        System.out.println("You wrote " + value);
+        */
+        /*
+        int number = 10;
+        modifyNumber(number);
+        System.out.println(number);
+        }
+        public static void modifyNumber(int number) {
+        number = number - 4;
+        */
+        /*
+        String[] ayIsimleri = {"ocak","Subat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylul","Ekim","Kasım","Aralık"};
+        int[] ay = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        for(int i =0 ; ayIsimleri.length >= 0; i++){
+        System.out.println(ay[i]+". ay = "+ayIsimleri[i]);
+    
+        }
+        */
+        /*
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        integers.add(15);
+        integers.add(34);
+        integers.add(65);
+        integers.add(111);
+        integers.add(25);
+        
+        System.out.println(integers.size());
+        */
+        /*
+        ArrayList<String> isimler = new ArrayList<>() ;
+        isimler.add("Cengiz");
+        isimler.add("Alperen");
+        isimler.add("Mehmet");
+        isimler.add("Ahmet");
+        System.out.println(isimler+"tüm isimler");
+        isimler.remove("Mehmet");
+        System.out.println(isimler+"çıkarılmış isimler");
+        ArrayList<Integer> plaka = new ArrayList<>() ;
+        plaka.add(06);
+        plaka.add(25);
+        plaka.add(15);
+        plaka.add(23);
+        System.out.println(plaka+"tüm plaka");
+        plaka.add(15);
+        System.out.println(plaka+"çıkarılmış plaka");
+        */
+        /*
+        static class BankAccount {
+        private String ad;
+        private double bakiye;
+
+        public  BankAccount(String ad, double bakiye) {
+        this.ad = ad;
+        this.bakiye = bakiye;
+        }
+
+        public void paraYatir(double miktar) {
+        if (miktar > 0) {
+            bakiye += miktar;
+            System.out.println(miktar + " TL yatırıldı.");
+        }
+        }
+
+        public void paraCek(double miktar) {
+        if (miktar > 0 && miktar <= bakiye) {
+            bakiye -= miktar;
+            System.out.println(miktar + " TL çekildi.");
+        } else {
+            System.out.println("Yetersiz bakiye.");
+        }
+        }
+
+        public void bakiyeGoster() {
+        System.out.println("Güncel bakiye: " + bakiye + " TL");
+        }
+
+        public static void main(String[] args) {
+        BankAccount hesap = new BankAccount("Ali", 1000);
+        hesap.paraYatir(500);
+        hesap.paraCek(300);
+        hesap.bakiyeGoster();
+        */
+        
+        }
+        }
+    
+
+
+
